74469df620f461043210b29ac1ea39b847e289fb
Jhonatan Kobylarz <38265015+Jhonkr@users.noreply.github.com>
2019-08-28 11:14:46 -0300

    Update ex1_jhonatan.asm
    Two routines to write A -> Z (hex) and 15 -> 50 (dec) into memory.

--- ex1_jhonatan.asm
+++ ex1_jhonatan.asm
@@ -25,10 +25,10 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here -> Write A to Z in ASCII and the number from 15 to 0 into the memory
 ;-------------------------------------------------------------------------------
-			clrz							; Clear the Z registrator
+			clrz					; Clear the Z registrator
 
-			call	#letter					; creating subroutine letter
-			call	#numbers				; creating subroutine numbers
+			call	#letter				; creating subroutine letter
+			call	#numbers			; creating subroutine numbers
 
 
 			jmp		$
@@ -37,28 +37,28 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 letter:		mov.b	#0x41, R10				; Atribute the A (0x041) to R10
 			mov.w 	#0x0220, R12			; Atribute the adress byte to R12 pointer
 
-loop:		mov.b	R10, 0(R12) 			; R12 as pointer and 0 as deslocation adress
-			cmp.b	#0x05A, R10				; Comparing if not Z -> if yes: Z = 1
-			jnz		incr					; Jump if Z is high
-			ret								; Exit subroutine
+loop:		mov.b	R10, 0(R12) 				; R12 as pointer and 0 as deslocation adress
+			cmp.b	#0x05A, R10			; Comparing if not Z -> if yes: Z = 1
+			jnz		incr			; Jump if Z is high
+			ret					; Exit subroutine
 
-incr:		inc.b	R10						; R10 = R10 + 1
-			inc.w	R12						; R12 = R12 + 1
-			jmp 	loop					; Back to loop
+incr:		inc.b	R10					; R10 = R10 + 1
+			inc.w	R12				; R12 = R12 + 1
+			jmp 	loop				; Back to loop
 
 ;-------------------------------------------------------------------------------
 
 numbers:	mov.b	#0x015, R10				; Atribute the number 15 (0x015) to R10
-			mov.w 	#0x0250, R12			; Atribute the adress byte to R12 pointer
+		mov.w 	#0x0250, R12				; Atribute the adress byte to R12 pointer
 
-loop1:      mov.b	R10, 0(R12) 			; R12 as pointer and 0 as deslocation
-			cmp.b	#0x050, R10				; Comparing const(#) 0x050 with R10
-			jnz		incr1   				; Jump to incr1 function
+loop1:      mov.b	R10, 0(R12) 				; R12 as pointer and 0 as deslocation
+			cmp.b	#0x050, R10			; Comparing const(#) 0x050 with R10
+			jnz	incr1   			; Jump to incr1 function
 			ret
 
 incr1:		dadd.b	#1, R10					; R10 = R10 + 1
 			inc.w	R12           			; R12 = R12 + 1
-			jmp		loop1					; Back to loop1
+			jmp	loop1				; Back to loop1
 
 
 ;-------------------------------------------------------------------------------
